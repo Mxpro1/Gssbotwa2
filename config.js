08bf1178907c9b902244c85a6bdad2720e3aacfc
Mxpro1 <154328653+Mxpro1@users.noreply.github.com>
2024-06-29 11:41:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,24 +8,24 @@ global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
 global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
-global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.owner = ['256705258548'];
+global.premium = ['256705258548'];
+global.packname = "PASSO";
+global.author = "FAMILY";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
 global.menuType = "2"
 global.autoswview = true
 global.autoread = false
-global.available = false
-global.autoTyping = false
+global.available = true
+global.autoTyping = true
 global.autoRecord = false
 global.autoreact = false
 global.onlygroup = false
 global.onlypc = false
 global.welcome = true
-global.autoBlock = true
+global.autoBlock = false
  
 /*REPLY MESSAGE*/
 global.mess = {
